d8a04d64510a7829169dcb4f9efab28671168128
Nini Antadze <nini.antadze@gmail.com>
2024-01-27 15:39:48 +0100

    added loading icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./lunasmagic-logo.svg";
 import "./App.css";
-
+import { Hearts } from "react-loader-spinner";
 export default function App() {
   return (
     <div className="App">
@@ -9,6 +9,15 @@ export default function App() {
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
           <h1>Coming Soon...</h1>
+          <Hearts
+            height="200"
+            width="150"
+            color="white"
+            ariaLabel="hearts-loading"
+            wrapperStyle={{}}
+            wrapperClass="d-flex justify-content-center align-items-center"
+            visible={true}
+          />
         </header>
       </div>
     </div>
