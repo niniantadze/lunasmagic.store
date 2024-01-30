26c789b8625fa3d74a249015ee2e817d5b4e2572
Nini Antadze <nini.antadze@gmail.com>
2024-01-30 20:11:06 +0100

    deleted coming soon content

--- src/App.js
+++ src/App.js
@@ -9,19 +9,7 @@ export default function App() {
       {" "}
       <Navbar />
       <div class="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-          <h1>Coming Soon...</h1>
-          <Hearts
-            height="200"
-            width="150"
-            color="white"
-            ariaLabel="hearts-loading"
-            wrapperStyle={{}}
-            wrapperClass="d-flex justify-content-center align-items-center"
-            visible={true}
-          />
-        </header>
+        <header className="App-header">/></header>
       </div>
     </div>
   );
